50f778bc6ac3415465775ac12b59b5be92aca31b
Samuel Underhill <sam@Samuels-MacBook-Pro.local>
2018-11-16 21:37:17 -0500

    get only significant countries

--- plot_wine.py
+++ plot_wine.py
@@ -2,6 +2,8 @@ import csv
 import numpy as np
 import pandas as pd
 from pandas.plotting import scatter_matrix
+import matplotlib as mpl
+mpl.use('TkAgg')
 import matplotlib.pyplot as plt
 from sklearn import model_selection
 from sklearn.metrics import classification_report
@@ -31,7 +33,11 @@ not_high_age = relevant_cols['vintage'] < "2019"
 
 relevant_cols_normal = relevant_cols[not_high_age & not_low_age]
 
-avg_points_by_vintage_country = relevant_cols_normal.groupby(['vintage', 'country']).agg(np.mean)
+sig_countries = relevant_cols_normal.groupby('country').filter(lambda g: (g['country'].value_counts() > 1000))
+
+avg_points_by_country = sig_countries.groupby(['country']).agg(np.mean)
+avg_points_by_vintage = sig_countries.groupby(['vintage']).agg(np.mean)
+avg_points_by_vintage_country = sig_countries.groupby(['vintage', 'country']).agg(np.mean)
 
 avg_points_by_vintage_country = avg_points_by_vintage_country.reset_index()
 
@@ -41,4 +47,7 @@ median_points = avg_points_by_vintage_country['points'].agg(np.median)
 
 avg_points_by_vintage_country['quality'] = np.where(avg_points_by_vintage_country['points'] > median_points, 1, 0)
 
-# print avg_points_by_vintage_country
+# print avg_points_by_vintage_country.query('country == "US"')
+print avg_points_by_country
+avg_points_by_country.plot(kind='bar')
+plt.show()
